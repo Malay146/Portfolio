b19b0f038862b0f2ae3c2f672120b811fc1caec9
MeetPatel-2005 <pmalay694@gmail.com>
2025-12-09 22:44:59 +0530

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: "https://malaypatel.dev",
+      url: "https://malaypatel.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1.0,
