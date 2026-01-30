011cf7bea29f83dacc3284cc0721fc02cab5371d
MeetPatel-2005 <pmalay694@gmail.com>
2026-01-31 00:21:37 +0530

    fixes

--- app/crafts/page.tsx
+++ app/crafts/page.tsx
@@ -165,7 +165,7 @@ const crafts = [
 
 const page = () => {
   return (
-    <div className="w-full border-x border-white flex flex-col lg:border-x-0">
+    <div className="w-full border-x border-white flex flex-col lg:border-x-0 relative overflow-y-hidden">
       <div className=" py-3 md:py-6 flex flex-col gap-3 md:gap-4">
         <div className="text-white flex justify-between items-center px-4 md:px-16 border-y border-dashed border-white/30">
           <h2 className="font-roboto-mono font-bold tracking-tighter text-[18px] md:text-[24px] px-2 border-x border-dashed border-white/30 relative hover:bg-white/10 transition-all duration-100 cursor-default">
